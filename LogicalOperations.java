5f52eb1361dec5a9b7b86b3cce814c61a4ecef9a
Sediqa Talibi <254766232+techGirl0@users.noreply.github.com>
2026-01-18 12:04:53 +0000

    Add cloud coverage weather classification exercise

--- LogicalOperations.java
+++ LogicalOperations.java
@@ -11,5 +11,18 @@ class LogicalOperations {
         boolean hasEnoughFuelForNextJourney = remainingFuel > (fuelCapacity / 2);
 
         System.out.println("Enough fuel for the next journey? " + hasEnoughFuelForNextJourney);
+
+        // Exercise: Weather Condition Classification Based on Cloud Coverage
+
+        int cloudCoveragePercentage = 30;
+
+        if (cloudCoveragePercentage < 20) {
+            System.out.println("clear skies");
+        } else if (cloudCoveragePercentage < 50) {
+            System.out.println("partly cloudy skies");
+        } else {
+            System.out.println("too cloudy skies");
+        }
+
     }
 }
